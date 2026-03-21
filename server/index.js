5842322bc3f185de4a2939384dea85781509acab
ricardoebrahim <blackadrenaline06@gmail.com>
2026-03-21 23:09:51 +0300

    debug: confirm auth route registeration

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 // Connect to MongoDB and start server
 app.use('/api/generate', generateRoute);
 app.use('/api/auth', authRoute);
+console.log('Auth route registered');
 app.use('/api/history', historyRoute);
 
 mongoose.connect(process.env.MONGO_URI)
